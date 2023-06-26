2b3d3d7335d597a57c25f0f308cb44e880931bda
Sachit <sachittarway2002@gmail.com>
2023-06-26 19:34:47 +0530

    Added some logic

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
 'use client';
-import { Card, Input, Row} from "@nextui-org/react";
+import { useState, useEffect } from "react";
+import { Card, Input, Row } from "@nextui-org/react";
 import { Button } from "@nextui-org/react";
-import { useState,useEffect } from "react";
 import Cards from "./cards";
 import Link from "next/link";
-import '../app/globals.css'
+import "../app/globals.css";
 import { Modal, useModal, Text } from "@nextui-org/react";
 
 export default function Home() {
@@ -14,150 +14,146 @@ export default function Home() {
   const [temp, setTemp] = useState("");
   const [ids, setIds] = useState("");
 
-  useEffect(()=>{
-    getData();
-  },[]);
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"));
+    if (todos) {
+      setWhole(todos);
+    }
+  }, []);
 
   const onChange = (e) => {
     setDes(e.target.value);
-  }
+  };
+
   const changed = (e) => {
     setTemp(e.target.value);
-  }
+  };
 
-  const getData = async() =>{
-    await fetch ("https://my-json-server.typicode.com/sachittarway/Todo/mydata",{
-        method:"GET"
-    })
-    .then((res)=>res.json())
-    .then((data)=>{console.log(data);
-        setWhole(data);
-    })
-  }
+  const saveTodosToLocalStorage = (todos) => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
-  const submitData = () =>{
-    fetch("https://my-json-server.typicode.com/sachittarway/Todo/mydata",{
-      method:"POST",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body:JSON.stringify({
-        description: des,
-        bool: false
-      })
-    })
-    .then((response)=>{
-      if(response.ok){
+  const submitData = () => {
+    const newTodo = {
+      description: des,
+      bool: false,
+    };
 
-        getData();
-        setDes("");
-      }
-    })
-  }
+    const updatedTodos = [...whole, newTodo];
+    setWhole(updatedTodos);
+    saveTodosToLocalStorage(updatedTodos);
 
-  const handleDelete = async(id) =>{
-    await fetch(`https://my-json-server.typicode.com/sachittarway/Todo/mydata/${id}`,{
-        method:"DELETE"
-    }).then((res)=>{
-      if(res.ok){
-        getData();
-      }
-    })
-  }
+    setDes("");
+  };
 
-  const handleupdate = async(id) =>{
-    console.log(id);
+  const handleDelete = (id) => {
+    const updatedTodos = whole.filter((todo) => todo.id !== id);
+    setWhole(updatedTodos);
+    saveTodosToLocalStorage(updatedTodos);
+  };
+
+  const handleUpdate = (id) => {
     setVisible(true);
     setIds(id);
-    await fetch(`https://my-json-server.typicode.com/sachittarway/Todo/mydata/${id}`,{
-      method:"GET"
-   }).then((res)=>res.json())
-   .then((data)=>{
-      setTemp(data.description);
-   })
-  }
 
-  const updates = async() =>{
-    await fetch(`https://my-json-server.typicode.com/sachittarway/Todo/mydata/${ids}`,{
-      method:"PUT",
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body:JSON.stringify({
-        description: temp,
-        bool: false
-      })
-    }).then((res)=>{
-      if(res.ok){
-        getData();
-        setVisible(false);
+    const todo = whole.find((item) => item.id === id);
+    setTemp(todo.description);
+  };
+
+  const updateTodo = () => {
+    const updatedTodos = whole.map((todo) => {
+      if (todo.id === ids) {
+        return {
+          ...todo,
+          description: temp,
+        };
       }
-    })
-  }
+      return todo;
+    });
 
-  const removed = () =>{
+    setWhole(updatedTodos);
+    saveTodosToLocalStorage(updatedTodos);
+
     setVisible(false);
     setIds("");
-  }
+  };
 
-  const handleCheck = async(id) =>{
-    await fetch(`http://localhost:8080/mydata/${id}`,{
-      method:"GET"
-   }).then((res)=>res.json())
-   .then((data)=>{
-      fetch(`http://localhost:8080/mydata/${id}`,{
-        method:"PUT",
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body:JSON.stringify({
-          description: data.description,
-          bool: !data.bool
-        })
-      }).then((res)=>{
-        if(res.ok){
-          getData();
-        }
-      })
-   })
-  }
+  const handleCheck = (id) => {
+    const updatedTodos = whole.map((todo) => {
+      if (todo.id === id) {
+        return {
+          ...todo,
+          bool: !todo.bool,
+        };
+      }
+      return todo;
+    });
+
+    setWhole(updatedTodos);
+    saveTodosToLocalStorage(updatedTodos);
+  };
+
   return (
-    <div className="parent" >
-      <Card className="card"css={{ mw: "700px" }} >
-        <Card.Header css={{alignItems:"center"}}>
+    <div className="parent">
+      <Card className="card" css={{ mw: "700px" }}>
+        <Card.Header css={{ alignItems: "center" }}>
           <Text b>TODO APP😊</Text>
         </Card.Header>
         <Card.Divider />
-        <div  style={{ display: 'flex',marginTop:"5px", marginLeft:"10px", marginBottom:"10px", marginTop:"20px"}}>
-          <Input clearable bordered width="500px" value={des} onChange={onChange} Placeholder="Description" name="desc" css={{ mw: "700px" }}/><br /><br />
-          <Button shadow color="success" onClick={submitData} auto css={{ mw: "100px", marginLeft:"10px"}}>Add Task</Button>
+        <div
+          style={{
+            display: "flex",
+            marginTop: "5px",
+            marginLeft: "10px",
+            marginBottom: "10px",
+            marginTop: "20px",
+          }}
+        >
+          <Input
+            clearable
+            bordered
+            width="500px"
+            value={des}
+            onChange={onChange}
+            Placeholder="Description"
+            name="desc"
+            css={{ mw: "700px" }}
+          />
+          <Button
+            shadow
+            color="success"
+            onClick={submitData}
+            auto
+            css={{ mw: "100px", marginLeft: "10px" }}
+          >
+            Add Task
+          </Button>
         </div>
-      
-        <div >
-          {
-            whole.length>0 ? 
-              (
-                <div className="originalcard"  >
-                  {
-                    whole.map((item)=>{
-                      return(
-                        <Cards id={item.id} className="singlecard" description={item.description} onclick={()=>handleDelete(item.id)} onUpdate={()=>handleupdate(item.id)} onCheck={()=>handleCheck(item.id)} buttonValue={item.bool}/>
-                      )
-                    })
-                  }
-                </div>
-              ):
-              (
-                <h1>ADD TASK</h1>
-              )
-          }
-        
+
+        <div>
+          {whole.length > 0 ? (
+            <div className="originalcard">
+              {whole.map((item) => (
+                <Cards
+                  id={item.id}
+                  className="singlecard"
+                  description={item.description}
+                  onclick={() => handleDelete(item.id)}
+                  onUpdate={() => handleUpdate(item.id)}
+                  onCheck={() => handleCheck(item.id)}
+                  buttonValue={item.bool}
+                />
+              ))}
+            </div>
+          ) : (
+            <h1>ADD TASK</h1>
+          )}
         </div>
         <Card.Footer className="footer">
           <Text>Created By Sachit💗</Text>
         </Card.Footer>
       </Card>
-      
+
       <Modal
         scroll
         width="600px"
@@ -172,19 +168,26 @@ export default function Home() {
         </Modal.Header>
         <Modal.Body>
           <Text id="modal-description">
-          <Input clearable bordered width="500px" value={temp} onChange={changed} labelPlaceholder="Description" name="desc" /> 
-            
+            <Input
+              clearable
+              bordered
+              width="500px"
+              value={temp}
+              onChange={changed}
+              labelPlaceholder="Description"
+              name="desc"
+            />
           </Text>
         </Modal.Body>
         <Modal.Footer>
           <Button auto flat color="error" onPress={() => removed()}>
             Close
           </Button>
-          <Button auto onPress={() => updates()}>
+          <Button auto onPress={() => updateTodo()}>
             Agree
           </Button>
         </Modal.Footer>
       </Modal>
     </div>
-  )
+  );
 }
